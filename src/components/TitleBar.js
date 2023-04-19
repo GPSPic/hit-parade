508a73c03f98aecf9b7ece120949cf6bf0514f4c
GPSPic <picard.guillaume47@gmail.com>
2023-04-19 15:06:56 +0100

    Add glib subtitle

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -4,6 +4,7 @@ const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
       <h1>CodeClan Hit Parade</h1>
+      <h2>Or how the music industry promotes lowest common denominator garbage to the top</h2>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
